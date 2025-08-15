99e13f7e4420ceb891c02259461d647d1043aeb6
areeb193 <fa22-bse-193@cuilahore.edu.pk>
2025-08-15 18:14:40 +0500

    done but did't deploy due to card issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Home from './components/Home';
-import Jobs from './components/Jobs';
+import Jobs from './components/jobs';
 import Browse from './components/Browse';
 import Profile from './components/Profile';
 import Companies from './components/admin/Companies';
